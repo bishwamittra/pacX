03e8cd7a4ff1f3076948edc6b9a294470ac6d137
bishwamittra <bishwa12buet@gmail.com>
2020-08-05 21:56:39 +0800

    untracked

--- trustable_explanation/teacher.py
+++ trustable_explanation/teacher.py
@@ -10,6 +10,8 @@ class Teacher():
         self.epsilon = epsilon
         self.delta = delta
         self.timeout = timeout
+        self.time_verifier = 0
+        self.time_learner = 0
 
     def teach(self, blackbox, learner, query, random_example_generator, params_generator, verbose = False):
 
@@ -20,14 +22,21 @@ class Teacher():
 
         for i in  range(self.max_iterations):
 
-            if(time.time() - _start_time > self.timeout):
+            if(verbose):
+                print("\n-iteration:", i + 1)
+            
+            
+            _start_time_vefirier = time.time()
+            if(_start_time_vefirier - _start_time > self.timeout):
                 print("\nTerminating due to timeout")
                 return learner, False
 
-            if(verbose):
-                print("\n-iteration:", i + 1)
-    
+            # ask for counterexample
             counterexample, label = self.verifier.equivalence_check(blackbox,learner,query, verbose=verbose)
+            
+            _end_time_verifier = time.time()
+            self.time_verifier += _end_time_verifier - _start_time_vefirier
+            
             if(counterexample == None):
                 if(verbose):
                     print("-no counterexample returned")
@@ -39,16 +48,18 @@ class Teacher():
 
 
                 # keep track of learner's fitting time
-                _start_learner = time.time()
-                if(self.timeout - (_start_learner - _start_time) > _past_learner_time):
+                _start_time_learner = time.time()
+                if(self.timeout - (_start_time_learner - _start_time) > _past_learner_time):
                     learner.fit()
                 else:
                     print("\nTerminating due to timeout")
                     if(verbose):
                         print("-we suspect that learner.fit() cannot finish in time")
-                        print("-time left: ", self.timeout - (_start_learner - _start_time), " whereas learner took", _past_learner_time, "last time")
+                        print("-time left: ", self.timeout - (_start_time_learner - _start_time), " whereas learner took", _past_learner_time, "last time")
                     return learner, False
-                _past_learner_time = time.time() - _start_learner 
+                _end_time_learner = time.time()
+                _past_learner_time = _end_time_learner - _start_time_learner 
+                self.time_learner += _end_time_learner - _start_time_learner
                 
 
         print("\nLearning complete\n-Total examples checked:", self.verifier.number_of_examples_checked)
